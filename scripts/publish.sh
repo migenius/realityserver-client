e19d2d8b34f6a5dd643ca7008b627ae07292019a
Brendan Hack <brendan@migenius.com>
2020-05-21 16:03:28 +1000

    add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$npm_package_version" == "" ]; then
+    echo "Not running in NPM, please run as 'npm run build:publish'"
+    exit 1
+fi
+
+# check we're on the release branch
+BRANCH=$(git branch --list release)
+
+if [ "$BRANCH" != "* release" ]; then
+    echo "Not on release branch, not publishing"
+    exit 0 # exit 0 so we don't cause npm to show errors
+fi
+
+# do a build to make sure we're up to date
+if npm run build; then
+    # and now publish
+    echo -n "Ready to publish ${npm_package_name}:${npm_package_version}, enter Y to continue: "
+    read line
+    if [ "$line" == "Y" ]; then
+        npm publish --access public
+    else
+        echo "Not publising since you entered '${line}'"
+    fi
+else
+    echo "Build failed, not publishing"
+fi
